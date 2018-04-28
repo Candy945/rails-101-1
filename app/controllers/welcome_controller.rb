5cebc8cbc7b0b4d1ac8acf325be3ea97d64a082f
Candy945 <525387153@qq.com>
2018-04-29 07:39:00 +0800

    creat groups index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,12 @@
 class WelcomeController < ApplicationController
+ def index
+    flash[:notice] = "早安！你好！"
+  end
+  def index
+    flash[:alert] = "晚安！该睡了！"
+  end
+  def index
+   flash[:warning] = "这是 warning 讯息！"
+  end
+
 end
