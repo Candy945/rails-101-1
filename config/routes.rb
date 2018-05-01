3caae0ab201c6ad0ecb1c65d4cebb1c4324a559a
Candy945 <525387153@qq.com>
2018-05-01 15:06:35 +0800

    implement my post

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :account do
     resources :groups
+    resources :posts
   end
-	
+
   resources :groups do
     member do
       post :join
